6d632905075f3638052a82e017eb38441c3c5e76
Zharkyn <zharkynabayev@MacBook-Air-Zharkyn.local>
2026-04-09 09:28:44 +0500

    Assignment 2

---
+++ src/six.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+public class six {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            result.add(curr.val);
+
+            curr = curr.right;
+        }
+
+        return result;
+    }
+}
---
+++ src/third.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+public class third {
+
+    public Stack<Integer> sortStack(Stack<Integer> input) {
+        Stack<Integer> tempStack = new Stack<>();
+
+        while (!input.isEmpty()) {
+            int temp = input.pop();
+
+            while (!tempStack.isEmpty() && tempStack.peek() < temp) {
+                input.push(tempStack.pop());
+            }
+            tempStack.push(temp);
+        }
+
+        while (!tempStack.isEmpty()) {
+            input.push(tempStack.pop());
+        }
+
+        return input;
+    }
+}
